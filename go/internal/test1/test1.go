5b66c6cc682c623d7f609afe4dc26557cba41d3b
GoidGoidGoida <megabebra228@gmail.com>
2024-02-03 14:38:35 +0300

    test commit

---
+++ go/internal/test1/test1.go
@@ -0,0 +1,49 @@
+package test1
+
+import (
+	"fmt"
+)
+
+type human struct {
+	age    int
+	name   string
+	status string
+}
+
+func (c human) GetAge() int {
+	return c.age
+}
+
+func (c *human) SetAge() int {
+	if age >= 0 && age <= 125 {
+		c.age = age
+		return nil
+	}
+	return fmt.Errorf("human named \"%s\" invalid age", c.GetName)
+}
+
+func (c human) GetName() string {
+	c.name = name
+}
+
+func (c *human) SetAge() string {
+	return c.name
+}
+
+func (c human) GetStatus() string {
+	return c.status
+}
+
+func (c *human) determineStatus(age int) {
+	if age < 13 {
+		c.status = "Kid"
+	} else if age >= 13 && age < 18 {
+		c.status = "Teenager"
+	} else if age >= 18 && age < 50 {
+		c.status = "Adult"
+	} else if age >= 50 && age < 80 {
+		c.status = "Pensioner"
+	} else {
+		c.status = "Dragon Ball Z"
+	}
+}
